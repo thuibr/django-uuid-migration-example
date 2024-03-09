511e2d2a79a1bdaf1df4a57aa26828d06c2a995e
Tom Huibregtse <t.huibr@gmail.com>
2024-03-09 06:12:11 -0600

    add uuid field

--- library/models.py
+++ library/models.py
@@ -1,7 +1,9 @@
+import uuid
 from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=255)
+    uuid = models.UUIDField(editable=False, unique=True, default=uuid.uuid4)
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
