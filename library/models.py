4ca8fa934547e510568fdad515203ecead620a98
Tom Huibregtse <t.huibr@gmail.com>
2024-03-10 07:34:58 -0500

    make uuid the author's pk while preserving id

--- library/models.py
+++ library/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=255)
-    uuid = models.UUIDField(editable=False, unique=True, default=uuid.uuid4)
+    uuid = models.UUIDField(primary_key=True, editable=False, unique=True, default=uuid.uuid4)
+    id = models.IntegerField()
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
