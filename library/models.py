e7070d2782fdf01280fb361094684cae7fb8d8f8
Tom Huibregtse <t.huibr@gmail.com>
2024-03-10 07:51:01 -0500

    make book's author not null again

--- library/models.py
+++ library/models.py
@@ -7,5 +7,5 @@ class Author(models.Model):
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
